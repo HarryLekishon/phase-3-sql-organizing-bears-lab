e677094b622f4b95a24cb883502e493a02f3496c
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-08-24 11:00:02 +0300

    final commit

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO bears (
+    name,
+    age,
+    sex,
+    color,
+    temperament,
+    alive
+) VALUES 
+(
+    'Mr. Chocolate',
+    5,
+    'F',
+    'Brown',
+    'Feisty',
+    TRUE
+),
+(
+    'Rowdy',
+    6,
+    'F',
+    'Brown',
+    'Mild',
+    TRUE
+),
+(
+    NULL,
+    7,
+    'F',
+    'Brown',
+    'Feisty',
+    TRUE
+);
